49de4f7495f29b5239788d6083d8789c58440529
kirso <kirill.sofronov@gmail.com>
2024-08-10 21:49:10 +0200

    font reference fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,7 +29,7 @@ export default {
 			},
 			fontFamily: {
 				// Add any custom fonts here
-				sans: ["AtkinsonHyperlegible", ...fontFamily.sans],
+				sans: ["Atkinson Hyperlegible", ...fontFamily.sans],
 			},
 			transitionProperty: {
 				height: "height",
