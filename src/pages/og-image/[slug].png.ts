8c84b7455a191a42414002a1e5c1de9128e30330
kirso <kirill.sofronov@gmail.com>
2024-12-31 16:31:24 +0700

    svg aspect ratio in OG image

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -42,7 +42,6 @@ const markup = (title: string, pubDate: string) =>
 					width="50"
 					height="50"
 					viewBox="0 0 50 50"
-					preserveAspectRatio="xMidYMid meet"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 				>
